c95c236fb512924a608ef0153154203c3a7af558
Fayçal <145764672+Fayzkr@users.noreply.github.com>
2026-06-16 15:05:56 +0200

    fix: align project setup with monolithic architecture

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,9 @@ const connectDB = require('./config/database.config');
 
 const app = express();
 app.use(express.json());
-app.use('/posts', require('./config/database.confg'));
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Post service is running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Breezy backend is running on port ${PORT}`));
 });
