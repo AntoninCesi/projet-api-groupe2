38fc852d7d2158979bd43a637e358ca9c94e478c
Fayçal <145764672+Fayzkr@users.noreply.github.com>
2026-06-18 10:58:18 +0200

    feat(post): add like, comment and reply with notifications (Fx6, Fx7, Fx8)

---
+++ backend/src/models/notification.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const notificationSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+    type: { type: String, enum: ['MENTION', 'LIKE', 'FOLLOW', 'REPOST', 'NEW_POST', 'TOPIC_ON_FIRE'], required: true},
+    sourceType: { type: String, required: true },
+    sourceId: { type: Schema.Types.ObjectId, required: true },
+    isRead: { type: Boolean, default: false },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Notification', notificationSchema);
